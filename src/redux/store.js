60989e0eb71b9f0b05c94fb3da8ee15147ed28df
Pranav Patil <pranav.patil@iitgn.ac.in>
2025-09-25 22:43:51 +0530

    added dashboard files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,39 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import authReducer from './auth/reducer';
-import dataReducer from './data/reducer';
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "./index.js";
 
-const rootReducer = combineReducers({
-  auth: authReducer,
-  data: dataReducer
-});
+function saveToLocalStorage(store) {
+  try {
+    const serializedStore = JSON.stringify(store);
+    window.localStorage.setItem("store", serializedStore);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+function loadFromLocalStorage() {
+  try {
+    const serializedStore = window.localStorage.getItem("store");
+    if (serializedStore === null) return undefined;
+    return JSON.parse(serializedStore);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistedState = loadFromLocalStorage();
+
+export const store = createStore(
+  rootReducer,
+  persistedState,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+
